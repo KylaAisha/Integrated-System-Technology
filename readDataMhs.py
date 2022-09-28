9a8d7e2120843c7cdeeafcca07cd226c02730c07
Kyla <114162261+KylaAisha@users.noreply.github.com>
2022-09-28 09:07:56 +0700

    Delete readDataMhs.py

--- readDataMhs.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, File, UploadFile
-import json
-app = FastAPI(debug = True)
-@app.post("/upload_files/")
-def create_upload_files(upload_file: UploadFile = File(...)):
-    json_data = json.load(upload_file.file);
-    return{"data_in_file": json_data}
